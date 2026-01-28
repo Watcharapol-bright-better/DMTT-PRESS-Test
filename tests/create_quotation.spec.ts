323eac0781abb6a6274e61bbbb0f38da9a9cfd33
BB-25004 <watcharapol@bright-better.com>
2026-01-28 17:25:29 +0700

    T200-C01-E01 | No Material Quotation Information Found

--- tests/create_quotation.spec.ts
+++ tests/create_quotation.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 import { TalonBase } from "./utils/talon_base";
 
-// Create Quotation (Press)
+
 test("T200-C01 | Create Quotation (3 Months Pattern)", async ({ page }) => {
   const testId = "T200-C01";
   const tln = new TalonBase(page);
@@ -38,28 +38,75 @@ test("T200-C01 | Create Quotation (3 Months Pattern)", async ({ page }) => {
 
   await tln.stepScreenshot(Ctx, testId, 4, "Input_PIC");
 
-  /* =========================
-     5) Select Exchange Rate Type
-     ========================= */
-  await Ctx.locator("#TLN_1_I_EXG_RATE_TYPE").selectOption("2");
-  await tln.stepScreenshot(Ctx, testId, 5, "Select_Exchange_Rate_Type");
 
   /* =========================
-     6) Click Get FG Details
+     5) Click Get FG Details
      ========================= */
   await tln.clickButton(Ctx, "Get FG Details");
-  await tln.stepScreenshot(Ctx, testId, 6, "Get_FG_Details");
+  await tln.stepScreenshot(Ctx, testId, 5, "Get_FG_Details");
 
   /* =========================
-     7) Submit
+     6) Submit
      ========================= */
   await tln.clickButton(Ctx, "Submit");
-  await tln.stepScreenshot(Ctx, testId, 7, "Submit");
+  await tln.stepScreenshot(Ctx, testId, 6, "Submit");
 
   /* =========================
-     8) Verify success message
+     7) Verify success message
      ========================= */
   await tln.expectMsg(Ctx, "info", "Successfully Submitted");
-  await tln.stepScreenshot(Ctx, testId, 8, "Message dialog");
+  await tln.stepScreenshot(Ctx, testId, 7, "Message dialog");
 
 });
+
+
+test("T200-C01-E01 | Create Quotation (3 Months Pattern) - No Material Quotation Information Found", async ({ page }) => {
+  const testId = "T200-C01-E01";
+  const tln = new TalonBase(page);
+
+  // Login
+  await tln.login();
+
+  // Navigate to Create Quotation (Press)
+  const Ctx = await tln.navigate(
+    "Sale Order (Press)",
+    "Create Quotation (Press)",
+    testId
+  );
+
+  /* =========================
+     1) Input Customer
+     ========================= */
+  const custPage = await tln.openPopup(Ctx, "#TLN_1_I_CSCODE_SUB");
+  await custPage.locator('input[name="R_ROW:11:j_idt96"]').click();
+
+  await tln.stepScreenshot(Ctx, testId, 1, "Input_Customer");
+
+  /* =========================
+     2) Input Pattern = 3 Months
+     ========================= */
+  await Ctx.locator("#TLN_1_I_TYPE").selectOption("0");
+  await tln.stepScreenshot(Ctx, testId, 2, "Input_Pattern_3_Months");
+
+  /* =========================
+     3) Input PIC
+     ========================= */
+  const picPage = await tln.openPopup(Ctx, "#TLN_1_I_PIC_SUB");
+  await picPage.locator('input[name="R_ROW:4:j_idt96"]').click();
+
+  await tln.stepScreenshot(Ctx, testId, 3, "Input_PIC");
+
+
+  /* =========================
+     4) Click Get FG Details
+     ========================= */
+  await tln.clickButton(Ctx, "Get FG Details");
+  await tln.stepScreenshot(Ctx, testId, 4, "Get_FG_Details");
+  
+  /* =========================
+     5) Expect Error Message
+     ========================= */
+  await tln.expectMsg(Ctx, "error", "Material price not found");
+  await tln.stepScreenshot(Ctx, testId, 5, "Expect_Error_Message");
+  
+} );
